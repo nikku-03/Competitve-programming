87131530cbd2ff5671b95ed343827883f96cc6cb
nikku-03 <151186121+nikku-03@users.noreply.github.com>
2024-03-26 11:02:33 +0530

    Create Pow(x,n).py

---
+++ Pow(x,n).py
@@ -0,0 +1,14 @@
+class Solution(object):
+  def myPow(self, x, n):
+    if n < 0:
+      return 1/self.power(x, -n)
+    else:
+      return self.power(x, n)
+  def power(self, x, n):
+    if n == 0:
+      return 1
+    v = self.power(x, n//2)
+    if n % 2 == 0:
+        return v * v
+    else:
+      return v * v * x
